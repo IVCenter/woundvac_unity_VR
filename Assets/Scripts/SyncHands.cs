dd0ac879548b6e8ab1e717da65009c3d2987f25a
AlexOuyang <ouyangchenxing@hotmail.com>
2016-11-04 18:39:25 -0700

    added LeftHand prefab to display on the AR side

---
+++ Assets/Scripts/SyncHands.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SyncHands : NetworkBehaviour {
+
+    private Transform viveHand;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isServer)
+        {
+            if (viveHand)
+            {
+                this.transform.position = viveHand.position;
+                this.transform.rotation = viveHand.rotation;
+            }
+            else
+            {
+                var go = GameObject.Find("Hand - Left");
+                if (go)
+                {
+                    viveHand = go.transform;
+                }
+            }
+        }
+	}
+}
