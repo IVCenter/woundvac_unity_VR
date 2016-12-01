5d38aef2717edf0b80941603d0ca23d75a82b461
AlexOuyang <ouyangchenxing@hotmail.com>
2016-11-30 20:35:07 -0800

    added GhostVacuum.cs

---
+++ Assets/Scripts/GhostVacuum.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GhostVacuum : NetworkBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent == null || transform.parent.tag != "VuforiaTracker")
+        {
+            GameObject tracker = GameObject.FindGameObjectWithTag("VuforiaTracker");
+            if (tracker != null)
+            {
+                transform.parent = tracker.transform;
+                transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+
+}
