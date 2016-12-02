20e798f5e9b9883144c690418fe6e9cb76bda617
AlexOuyang <ouyangchenxing@hotmail.com>
2016-12-02 05:20:30 -0800

    Added a new client scene for testing purpose

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -2,19 +2,29 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class Move : NetworkBehaviour
+public class MoveTest : NetworkBehaviour
 {
-
+    public enum AUTHORITY {
+        NONE,
+        CLIENT_MOVE,
+        SERVER_MOVE,
+    }
+    public AUTHORITY authority = AUTHORITY.CLIENT_MOVE;
     public float movementSpeed = 10f;
     public float rotationSpeed = 100f;
 
     void Update()
     {
-        if (!isServer) return;
+        if (authority == AUTHORITY.NONE) return;
+        if (authority == AUTHORITY.CLIENT_MOVE && isClient) Move();
+        if (authority == AUTHORITY.SERVER_MOVE && isServer) Move();
+    }
 
+    private void Move()
+    {
         float translation = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-
+        
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
 
